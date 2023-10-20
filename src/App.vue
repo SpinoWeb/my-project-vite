734a63934b547199ed31a948a9d435f375a4305c
SpinoWeb <spinoweb@hotmail.com>
2023-10-20 19:12:16 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -108,26 +108,13 @@
     </div>
   </div>
 
-  <div class="container mx-auto rounded-xl shadow border p-8 m-10">
-    <article class="prose">
-      <h1>Garlic bread with cheese: What the science tells us</h1>
-      <p>
-        For years parents have espoused the health benefits of eating garlic
-        bread with cheese to their children, with the food earning such an
-        iconic status in our culture that kids will often dress up as warm,
-        cheesy loaf for Halloween.
-      </p>
-      <p>
-        But a recent study shows that the celebrated appetizer may be linked to
-        a series of rabies cases springing up around the country.
-      </p>
-      <!-- ... -->
-    </article>
-  </div>
-
-  <div class="container mx-auto rounded-xl shadow border p-8 m-10">
-    <div class="grid grid-cols-12 gap-2">
-      <button class="btn btn-primary" v-for="i in 36">{{ i }}</button>
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
     </div>
   </div>
 </template>
